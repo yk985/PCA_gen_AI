92d444a1a2be1c22b4cddfae60330e678a49552c
Marzio Formica <marzio.formica@epfl.ch>
2025-07-18 13:44:00 +0200

    to_date

--- CODE/AttentionDCA_python/src/PLM/txt_to_fasta.py
+++ CODE/AttentionDCA_python/src/PLM/txt_to_fasta.py
@@ -13,15 +13,19 @@ seq_dir = 'generated_sequences'
 seq_dir = 'gill_generated_sequences'
 #filename = 'gill_gen_seqs_w_init_seq_Ns30000_r0.3_after_mod'
 filename = 'gill_generated_sequences_randinit_20000'
+
+seq_dir = 'generated_sequences_brute_forcePCA_comp'
+filename = 'gen_seqs_randinit_Ns15000_b_1_b_PCA10_PCA_comp_24_20PCA_comp'
+
 output_file = f'{seq_dir}/{filename}.npy'
 gill_gen_seqs = np.load(output_file)
 
-# convert sequences to a list of strings using nums_to_letters
-gill_gen_seqs = [nums_to_letters(seq) for seq in gill_gen_seqs]
-# Save the sequences to a text file
-with open(f'{seq_dir}/{filename}.txt', 'w') as outfile:
-    for seq in gill_gen_seqs:
-        outfile.write(f"{seq}\n")
+## convert sequences to a list of strings using nums_to_letters
+#gill_gen_seqs = [nums_to_letters(seq) for seq in gill_gen_seqs]
+## Save the sequences to a text file
+#with open(f'{seq_dir}/{filename}.txt', 'w') as outfile:
+#    for seq in gill_gen_seqs:
+#        outfile.write(f"{seq}\n")
 
 
 txt_file = f'{seq_dir}/{filename}.txt'
@@ -33,6 +37,9 @@ with open(txt_file, 'r') as infile:
 # Make sure there are enough sequences
 if len(sequences) <= 5000:
     raise ValueError("Not enough sequences to start at index 5000!")
+# Check size of a sequence
+if len(sequences[0]) != 63:
+    raise ValueError("Sequences are not of length 63, please check the input file.")
 
 # Select sequences starting from index 5000
 selected_sequences = sequences[5000:]
